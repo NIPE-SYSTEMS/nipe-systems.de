c6f783341421a42a05bd9c197c58c297679b8136
NIPE-SYSTEMS <dev.nipe.systems@gmail.com>
2018-02-14 21:58:58 +0100

    Replace discount with pandoc

--- convert-texts.sh
+++ convert-texts.sh
@@ -16,6 +16,6 @@ echo "Generating $OUTPUT_FILE ($PROJECT)..."
 LINE=$(grep -n {{text}} template_text.html | cut -d : -f1)
 cp "$TEMPLATE_FILE" "$OUTPUT_FILE"
 head -n $(echo $LINE - 1 | bc -l) "$TEMPLATE_FILE" > "$OUTPUT_FILE"
-markdown -f +links,+image,+html,+autolink "$INPUT_FILE" >> "$OUTPUT_FILE"
+pandoc --from gfm+tex_math_dollars --to html --highlight-style tango "$INPUT_FILE" >> "$OUTPUT_FILE"
 tail -n +$(echo $LINE + 1 | bc -l) "$TEMPLATE_FILE" >> "$OUTPUT_FILE"
 sed -i "s|<title>NIPE-SYSTEMS</title>|<title>NIPE-SYSTEMS - $(basename "$(dirname "$INPUT_FILE")")</title>|g" "$OUTPUT_FILE"
