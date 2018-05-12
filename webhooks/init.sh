f7cc6a9ab4c34fc3dbe3fab381eb630a1d1de308
NIPE-SYSTEMS <dev.nipe.systems@gmail.com>
2018-05-12 19:10:22 +0200

    Remove unused files

--- webhooks/init.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-ssh-keygen -b 4096 -t rsa -C "github-website-update@intranet.nipe-systems.de" -N "" -f github-website-update
-mv github-website-update{,.key}
-git clone https://github.com/carlos-jenkins/python-github-webhooks.git
-docker build -t github-website-update .
